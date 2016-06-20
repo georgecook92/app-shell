c635680fed5c8f4aefc6c9d6fc21edd222ea5581
George <george.cook.92@hotmail.co.uk>
2016-06-20 13:08:08 +0100

    change sw - add cache

--- sw.js
+++ sw.js
@@ -1,16 +1,48 @@
-var cacheName = 'v2';
-var filesToCache = [
-  '/',
-  'index.html',
-  'inlined.css'
-];
+var CACHE_NAME = 'v3';
 
-self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
+//install event
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+    caches.open(CACHE_NAME).then( (cache) => {
+      console.log("Cache has been opened");
+      console.log("another New SW");
+      return cache.addAll(CACHE_ARRAY);
     })
   );
 });
+
+//activate - cleans up old caches if they are still around
+self.addEventListener( 'activate' , (event) => {
+  var cacheWhitelist = CACHE_NAME;
+
+  event.waitUntil(
+    caches.keys().then( (keyList) => {
+      return Promise.all( keyList.map( (key) => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+
+//fetch event
+self.addEventListener( 'fetch', (event) => {
+
+  event.respondWith(
+    caches.match(event.request)
+    .then((response) => {
+      //if response is truthy return it - else fetch the network
+      //if error in network call the fallback
+      return response || fetch(event.request).catch( () => {
+        return new Response( "<h1> This is the offline fallback </h1>" , {
+          headers: {
+            'Content-Type' : 'text/html'
+          }
+        })
+      } );
+
+    })
+  );
+} );
